5c43a839f709a39eb63fc59363b9fd191f9fe3d6
Nathan Lyum <nlyum@uci.edu>
2024-04-25 23:03:10 -0700

    created working test to create a file

--- pathlib_test.py
+++ pathlib_test.py
@@ -3,5 +3,9 @@ from shlex import split
 
 if __name__ == "__main__":
     path1 = Path("/Users/natha/OneDrive/Desktop/UCI/ICS 32")
-    file1 = path1 / ("test_create" + ".dsu")
-    file1.mkdir()
+    path1.mkdir(exist_ok = True)
+    
+    filename1 = input("Filename: ")
+    filepath1 = path1 / filename1
+    f = filepath1.open("w")
+    f.close
